c8562d271ad33a3d0c035e6b8dc322498643ec74
Miyu <camillemaurice@gmail.com>
2015-09-22 15:12:17 +0200

    corrected version

--- test2.m
+++ test2.m
@@ -1,50 +1,52 @@
 function flow = test2()
 
-% TEST2 Demonstrates gradient-based image
-%   min-cut partitioning.
-%
-%   (c) 2008 Michael Rubinstein, WDI R&D and IDC
-%   $Revision: 140 $
-%   $Date: 2008-09-15 15:35:01 -0700 (Mon, 15 Sep 2008) $
-%
-
 im = imread('cow.jpeg');
 %im = imread('Lenna.png');
-figure(1)
-imshow(rgb2gray(im))
+%figure(1)
+%imshow(rgb2gray(im))
 
 %normalized greyscale image
 
-m = double(rgb2gray(im)/255);
+m = double(rgb2gray(im))/255;
+figure(1)
+imshow(m)
 
 %Convert image matrix into vector
 reshape([m], [], 1);
 
 [height,width] = size(m);
 
-disp('building graph');
 N = height*width;
 
 % construct graph
 E = edges4connected(height,width);
 
+%prior avec distribution de couleurs avec histogrammes
+%data term distance a lhistogramme
+% prendre probabilite dun pxiel de couleurs dans lhistogramme.
+% groupe de 16/8 pour la couleur (8*8*8)
+
 %assuming the object is black and the background is white
 costObj = abs(m);
 costBkg = abs(1-m); 
 imagesc(costObj)
+
 %data term
 T = [reshape([costObj], [], 1) , reshape([costBkg], [], 1) ];
 T = sparse(T);
 
 %smoothness term
-alpha = 0.5 ;
+alpha = 0.8 ;
 sigma = 0.5 ;
 
 Intensity_pq = [m(E(:,1)), m(E(:,2))];
 Intensity_pq_diff = abs(Intensity_pq(:,1)-Intensity_pq(:,2)).*abs(Intensity_pq(:,1)-Intensity_pq(:,2));
-distance = abs(E(:,1)-E(:,2));
+%Repasser les E(:,) e
+[idx,idy] = ind2sub([height, width], E(:,1));
+[idx2,idy2] = ind2sub([height, width], E(:,2));
 
-%needs to be checked it doesnt change anything
+distance = sqrt((idx-idx2).^2 + (idy-idy2).^2);
+
 B = (alpha*(exp(-Intensity_pq_diff))/(2*sigma*sigma))./distance;
 
 A = sparse(E(:,1),E(:,2),B);
@@ -55,5 +57,8 @@ disp('calculating maximum flow');
 labels = reshape(labels,[height width]);
 
  figure(2)
- imagesc(labels); title('labels // Alpha = 0.2 sigma = 1');
-  %colormap(gray)
+  imshow(m);
+ hold on;
+  %imagesc(labels); title(['labels // Alpha =',num2str(alpha),'sigma =', num2str(sigma)]);
+ colormap(gray)
+ imcontour(uint8(labels))
