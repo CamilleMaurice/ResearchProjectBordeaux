6f0295b28df1cdd61f2e1fd991b0dc816c4006f1
Charlotte Assemat <cassemat@emi.u-bordeaux1.fr>
2015-10-28 15:18:58 +0100

    histo fonctionne avec image vectorisee 1D; fichiers GUI remontes pour que test2 soit trouve

--- histo3D.m
+++ histo3D.m
@@ -1,22 +1,13 @@
 %3D histogram
 %bins = 8
 function histo = histo3D(image,n_bins)
-w_bins=256/n_bins
-[w,h,z] = size(image);
+w_bins=256/n_bins;
+[l,z] = size(image)
 histo=zeros(n_bins,n_bins,n_bins);
-for i=1:w
-    for j=1:h
-        image(i,j,:);
-%         image(i,j,1)/w_bins
-%         floor(image(i,j,1)/w_bins)
-%         bin_r=floor(image(i,j,1)/w_bins)+1;
-%         bin_r=(image(i,j,1)-mod(image(i,j,1),w_bins))/n_bins;
-        bin_r=floor(double(image(i,j,1))/double(w_bins))+1;
-        bin_g=floor(double(image(i,j,2))/double(w_bins))+1;
-        bin_b=floor(double(image(i,j,3))/double(w_bins))+1;
-%         bin_g=floor(image(i,j,2)/w_bins);
-%         bin_b=floor(image(i,j,3)/w_bins);
+for i=1:l
+        bin_r=floor(double(image(l,1))/double(w_bins))+1;
+        bin_g=floor(double(image(l,2))/double(w_bins))+1;
+        bin_b=floor(double(image(l,3))/double(w_bins))+1;
         histo(bin_r,bin_g,bin_b) = histo(bin_r,bin_g,bin_b) +1;
-    end
 end
 end
